09c8cb4e69e71bf5cf0983e1861071b0a8a08c28
MrRaccxxn <ayrtonskt@gmail.com>
2023-01-23 20:39:30 -0400

    Wallet Connector Style

--- src/common/layout/NavBar.tsx
+++ src/common/layout/NavBar.tsx
@@ -1,8 +1,9 @@
 import _ from "lodash";
+import classNames from "classnames";
 import { useWeb3Auth } from "../context/web3AuthContext";
 
 export const NavBar = () => {
-    const { publicKey, login, isLoading } = useWeb3Auth()
+    const { publicKey, login, logout, isLoading } = useWeb3Auth()
 
     const handleLogin = async () => {
         await login();
@@ -12,45 +13,35 @@ export const NavBar = () => {
         <div className="flex-1">
             <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
         </div>
-        <div className="flex-none">
-            <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle">
-                    <div className="indicator">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                        <span className="badge badge-sm indicator-item">8</span>
-                    </div>
-                </label>
-                <div tabIndex={0} className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
-                    <div className="card-body">
-                        <span className="font-bold text-lg">8 Items</span>
-                        <span className="text-info">Subtotal: $999</span>
-                        <div className="card-actions">
-                            <button className="btn btn-primary btn-block">View cart</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            {
-                _.isEmpty(publicKey) ? <button className="btn" onClick={handleLogin}>Login</button> : <p>{publicKey}</p>
-            }
 
-            {/* <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                        <img src="https://placeimg.com/80/80/people" />
-                    </div>
-                </label>
-                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                    <li>
-                        <a className="justify-between">
-                            Profile
-                            <span className="badge">New</span>
-                        </a>
-                    </li>
-                    <li><a>Settings</a></li>
-                    <li><a>Logout</a></li>
-                </ul>
-            </div> */}
-        </div>
+        {
+            _.isEmpty(publicKey) ?
+                (
+                    <button className={classNames([
+                        'btn',
+                        isLoading ? '' : '',
+                    ])} onClick={handleLogin}>Login</button>
+                )
+                :
+                (
+                    <button onClick={logout}>
+                        <div className="flex flex-row gap-3 items-center">
+                            <div className="avatar">
+                                <div className="w-10 rounded-full">
+                                    <img src="https://placeimg.com/192/192/people" />
+                                </div>
+                            </div>
+                            <div className="font-medium">
+                                <div className="text-white">
+                                    {publicKey.slice(0, 5) + '...' + publicKey.slice(publicKey.length - 5, publicKey.length)}
+                                </div>
+                                <div className="text-sm text-gray-500 dark:text-gray-400">
+                                    Dungeon Master
+                                </div>
+                            </div>
+                        </div>
+                    </button>
+                )
+        }
     </div>
 }
